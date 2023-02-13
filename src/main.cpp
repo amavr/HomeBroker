0e4e2c09c5405949c077e7ba4e519b1cf2412c97
amavr <albert.mavrin@gmail.com>
2023-02-14 02:50:21 +0300

    Big refactoring

--- src/main.cpp
+++ src/main.cpp
@@ -3,23 +3,73 @@
 #include <SubList.h>
 #include <TopicList.h>
 #include <EventBroker.h>
+#include <WiFiController.h>
+#include <UDPRequest.h>
+#include <ATools.h>
+#include <GParser.h>
 
 EventBroker broker;
+WiFiController ctrl;
 
-void cbSub(const char *id)
-{
-    Serial.printf("  %s\n", id);
-}
+#include "EspNowConnector.h"
+EspNowConnector conn;
 
-void cbTopic(Topic *topic)
-{
-    Serial.println(topic->name);
-}
+uint16_t sizeEEPROM = 512;
 
-void cbTopicSubs(Topic *topic)
+// u8 *mac_addr, u8 *data, u8 len
+void OnDataRecv(uint8_t *mac, uint8_t *data, uint8_t len)
 {
-    Serial.println(topic->name);
-    topic->subscribers->forEach(&cbSub);
+    char text[len];
+    char rest[len];
+    ATools::normalize((char *)data, text);
+    Serial.println(text);
+
+    // subscription command ("sub <topic>")
+    if (ATools::isCmd("sub ", text, rest))
+    {
+        GParser data(rest, ' ');
+        int count = data.split();
+        if (count == 1)
+        {
+            char addr[18];
+            ATools::macToChars(mac, addr);
+            broker.subscribe(addr, rest);
+        }
+    }
+    // subscription command ("pub <topic>:<any data>")
+    else if (ATools::isCmd("pub ", text, rest))
+    {
+        // отделить данные от топика
+        GParser data(rest, ':');
+        int count = data.split();
+        if (count == 2)
+        {
+            // выделить топик
+            GParser ldata(data[0], ' ');
+            int lcount = ldata.split();
+            if (lcount == 2)
+            {
+                // опубликовать данные для топика
+                SubList *subs = broker.publish(ldata[1], data[1]);
+                // получить подписчиков
+                if (subs->Count() > 0)
+                {
+                    int count = subs->Count();
+                    char *ids[count];
+                    subs->fill(ids);
+                    uint8_t mac[6];
+                    // разослать данные топика подписчикам
+                    for (int i = 0; i < count; i++)
+                    {
+                        ATools::macToBytes(ids[i], mac);
+                        // "pub <topic>:<any data>"
+                        // except "pub "
+                        esp_now_send(mac, (uint8_t *)(text + 4), strlen(text) - 4);
+                    }
+                }
+            }
+        }
+    }
 }
 
 void setup()
@@ -27,46 +77,43 @@ void setup()
     Serial.begin(115200);
     delay(100);
 
-    Serial.println("started");
+    EEPROM.begin(sizeEEPROM);
 
-    // TopicList topics;
-    // Topic *t = topics.add("test");
+    // Признак первого запуска
+    bool isFirstTime = EEPROM[0] != 0x22;
+    Serial.printf("isFirstTime: %d\n", isFirstTime);
 
-    // t->subscribers->add("111");
-    // t->subscribers->add("111");
-    // t->subscribers->add("222");
-    // Serial.printf("subs count %d\n", t->subscribers->Count());
-    // t->subscribers->forEach(&cbSub);
+    // подключение к WiFi
+    ctrl.connect(isFirstTime);
 
-    // topics.add("test-2");
-    // topics.add("test-3");
-    // Serial.printf("topic count %d\n", topics.Count());
-    // topics.forEach(&cbTopic);
+    // после инициализации подключения и старта информера
+    // EEPROM проинициализирована, поэтому сброс флага первого запуска
+    if (isFirstTime)
+    {
+        EEPROM[0] = 0x22;
+        EEPROM.commit();
+    }
 
-    broker.Subscribe("first", "OUTSIDE.TEMP");
-    broker.Subscribe("second", "OUTSIDE.TEMP");
-    broker.Subscribe("first", "INSIDE.TEMP");
-    broker.forEachTopic(&cbTopicSubs);
+    // регистрация в шлюзе
+    setBrokerAddr();
+    char addr[0x20];
+    if (getBrokerAddr(addr))
+    {
+        Serial.println(addr);
+    }
+
+    // запуск протокола ESP-NOW
+    conn.setReceiveCallback(OnDataRecv);
+
+    // broker.Subscribe("first", "OUTSIDE.TEMP");
+    // broker.Subscribe("second", "OUTSIDE.TEMP");
+    // broker.Subscribe("first", "INSIDE.TEMP");
+    // broker.forEachTopic(&cbTopicSubs);
 }
 
 void loop()
 {
-    for(int i = 0; i < 1000; i++){
-        long x = random(0, 10);
-        const char *topicName = x < 6 ? "OUTSIDE.TEMP" : "INSIDE.TEMP";
-
-        char data[20] = "";
-        ultoa(millis(), data, DEC);
-
-        // Serial.printf("\n%s - %s\n", topicName, data);
-
-        SubList *subs = broker.newEvent(topicName, data);
-        // subs->forEach(&cbSub);
-    }
-
-    uint32_t freeHeap = ESP.getFreeHeap();
-    uint8_t frag = ESP.getHeapFragmentation();
-    Serial.printf("%ld\t%d\t%d\n", millis(), frag, freeHeap);
-
+    // проверка сети
+    ctrl.tick();
     delay(1000);
 }
