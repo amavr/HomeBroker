548ff9247b821196e5c548262cab6f3e28e03f78
amavr <albert.mavrin@gmail.com>
2023-02-16 02:21:23 +0300

    ss

--- src/main.cpp
+++ src/main.cpp
@@ -19,7 +19,8 @@ uint16_t sizeEEPROM = 512;
 // u8 *mac_addr, u8 *data, u8 len
 void OnDataRecv(uint8_t *mac, uint8_t *data, uint8_t len)
 {
-    char text[len];
+    data[len] = 0;
+    char text[len + 1];
     char rest[len];
     ATools::normalize((char *)data, text);
     Serial.println(text);
@@ -31,12 +32,16 @@ void OnDataRecv(uint8_t *mac, uint8_t *data, uint8_t len)
         int count = data.split();
         if (count == 1)
         {
+            // со всеми подписчиками устанавливается связь
+            conn.pair(mac);
+
             char addr[18];
             ATools::macToChars(mac, addr);
             broker.subscribe(addr, rest);
         }
     }
     // subscription command ("pub <topic>:<any data>")
+    // rest = "<topic>:<any data>"
     else if (ATools::isCmd("pub ", text, rest))
     {
         // отделить данные от топика
@@ -44,32 +49,35 @@ void OnDataRecv(uint8_t *mac, uint8_t *data, uint8_t len)
         int count = data.split();
         if (count == 2)
         {
-            // выделить топик
-            GParser ldata(data[0], ' ');
-            int lcount = ldata.split();
-            if (lcount == 2)
+            // опубликовать данные для топика
+            SubList *subs = broker.publish(data[0], data[1]);
+            int count = subs->Count();
+            // получить подписчиков
+            if (count > 0)
             {
-                // опубликовать данные для топика
-                SubList *subs = broker.publish(ldata[1], data[1]);
-                // получить подписчиков
-                if (subs->Count() > 0)
+                char *ids[count];
+                subs->fill(ids);
+                // uint8_t mac[6];
+                // разослать данные топика подписчикам
+                for (int i = 0; i < count; i++)
                 {
-                    int count = subs->Count();
-                    char *ids[count];
-                    subs->fill(ids);
-                    uint8_t mac[6];
-                    // разослать данные топика подписчикам
-                    for (int i = 0; i < count; i++)
-                    {
-                        ATools::macToBytes(ids[i], mac);
-                        // "pub <topic>:<any data>"
-                        // except "pub "
-                        esp_now_send(mac, (uint8_t *)(text + 4), strlen(text) - 4);
-                    }
+                    conn.send(ids[i], (char *)(text + 4));
+                    // ATools::macToBytes(ids[i], mac);
+                    // // "<topic>:<any data>"
+                    // esp_now_send(mac, (uint8_t *)(text + 4), strlen(text) - 4);
+                }
+                for (int i = 0; i < count; i++)
+                {
+                    free(ids[i]);
                 }
             }
         }
     }
+}
+
+void OnDataSent(uint8_t *mac, uint8_t status)
+{
+    Serial.printf("%s\n", status == 0 ? "success" : "fail");
 }
 
 void setup()
@@ -83,6 +91,7 @@ void setup()
     bool isFirstTime = EEPROM[0] != 0x22;
     Serial.printf("isFirstTime: %d\n", isFirstTime);
 
+
     // подключение к WiFi
     ctrl.connect(isFirstTime);
 
@@ -94,6 +103,9 @@ void setup()
         EEPROM.commit();
     }
 
+    // восстановление подписок
+    broker.load();
+
     // регистрация в шлюзе
     setBrokerAddr();
     char addr[0x20];
@@ -102,18 +114,17 @@ void setup()
         Serial.println(addr);
     }
 
-    // запуск протокола ESP-NOW
-    conn.setReceiveCallback(OnDataRecv);
+    WiFi.disconnect();
 
-    // broker.Subscribe("first", "OUTSIDE.TEMP");
-    // broker.Subscribe("second", "OUTSIDE.TEMP");
-    // broker.Subscribe("first", "INSIDE.TEMP");
-    // broker.forEachTopic(&cbTopicSubs);
+    // запуск протокола ESP-NOW
+    conn.start();
+    conn.setReceiveCallback(OnDataRecv);
+    conn.setSendCallback(OnDataSent);
 }
 
 void loop()
 {
-    // проверка сети
-    ctrl.tick();
+    // проверка сети (брокер должен работать без WiFi)
+    // ctrl.tick();
     delay(1000);
 }
